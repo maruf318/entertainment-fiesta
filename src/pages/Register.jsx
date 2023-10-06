50be786ed44bd3ef6fc71193220004dc558e08ea
Maruf Hossain <hossainmaruf001@gmail.com>
2023-10-06 20:33:36 +0200

    added navbar section

---
+++ src/pages/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div>
+      <h3 className="text-4xl">This is Register page</h3>
+    </div>
+  );
+};
+
+export default Register;
--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,8 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root/Root";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 
